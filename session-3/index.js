62ea055d1b108dac99097a9d364def143398b50d
lemuela888 <888lemuela@gmail.com>
2021-11-29 19:57:14 +0000

    Task 2 changes ('new changes' was Task 1)

--- session-3/index.js
+++ session-3/index.js
@@ -2,18 +2,14 @@
 // You can see where this file is connected to your HTML in the index.html on line 14
 
 
-let a = 5;
-const b = 10;
-let c = a + b;
+function sayHey(){
+    console.log("Hey!");
+}
 
-console.log(c);
+function conversation(){
+    sayHey();
+    console.log("How are you?");
+    console.log("Goodbye");
+}
 
-a = 20;
-
-console.log('after a has been updated' + c);
-
-c = a + b;
-
-console.log('after a has been updated with the new a value' + c);
-
-console.log(c);
+conversation();
