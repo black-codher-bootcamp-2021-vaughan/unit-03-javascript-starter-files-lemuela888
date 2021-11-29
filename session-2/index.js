c97662c67c48c1e35fd4b36b32ede38a30f67c05
lemuela888 <888lemuela@gmail.com>
2021-11-29 19:22:10 +0000

    New changes

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,5 @@
+console.log('hello everyone!');
+console.log(document.querySelector('.about').innerHTML);
+console.log(document.querySelectorAll('.about'));
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
